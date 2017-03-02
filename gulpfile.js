6d1293c257d9a45b879424cb68d74762e3666b2e
Matt Bates <matt.bates@manywho.com>
2017-03-02 11:54:15 +0000

    CORE-3086 - Added pre-commit hook to lint typescript

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@ var plugins = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
 var del = require('del');
 var argv = require('yargs').argv;
-var path = require('path'); 
+var path = require('path');
 
 function getTask(task) {
     return require('./gulp-tasks/' + task)(gulp, plugins, argv);
 }
+
+// Hooks
+gulp.task('pre-commit', getTask('hooks/pre-commit'));   
 
 // Dev
 gulp.task('dev-ts', getTask('dev/ts'));
