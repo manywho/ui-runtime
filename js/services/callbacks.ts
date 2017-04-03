7f2b27aeca3783a543a1a1d211ecabcbab5e3086
Matt Bates <Matt Bates>
2017-04-03 15:09:08 +0100

    CORE-3142 - Converted callbacks service to typescript

--- js/services/callbacks.ts
+++ js/services/callbacks.ts
@@ -1,78 +1,54 @@
+/// <reference path="../../typings/index.d.ts" />
+
+declare var manywho: any;
+
 manywho.callbacks = (function (manywho) {
 
-    var callbacks = {};
+    const callbacks = {};
 
     return {
 
         register: function (flowKey, options) {
-
-            var lookUpKey = manywho.utils.getLookUpKey(flowKey);
+            const lookUpKey = manywho.utils.getLookUpKey(flowKey);
 
             callbacks[lookUpKey] = callbacks[lookUpKey] || [];
 
-            if (!options.flowKey) {
-
+            if (!options.flowKey)
                 options.flowKey = flowKey;
 
-            }
-
             callbacks[lookUpKey].push(options);
-
         },
 
         execute: function (flowKey, type, name, mapElementId, args) {
+            const lookUpKey = manywho.utils.getLookUpKey(flowKey);
 
-            var lookUpKey = manywho.utils.getLookUpKey(flowKey);
-
-            if (callbacks[lookUpKey]) {
-
+            if (callbacks[lookUpKey])
                 callbacks[lookUpKey].filter(function (item) {
 
-                    if (type && !manywho.utils.isEqual(item.type, type, true)) {
-
+                    if (type && !manywho.utils.isEqual(item.type, type, true))
                         return false;
 
-                    }
-
-                    if (name && !manywho.utils.isEqual(item.name, name, true)) {
-
+                    if (name && !manywho.utils.isEqual(item.name, name, true))
                         return false;
 
-                    }
-
-                    if (!manywho.utils.isEqual(type, 'done', true) && mapElementId && !manywho.utils.isEqual(item.mapElement, mapElementId, true)) {
-
+                    if (!manywho.utils.isEqual(type, 'done', true) && mapElementId && !manywho.utils.isEqual(item.mapElement, mapElementId, true))
                         return false;
-
-                    }
 
                     return true;
-
                 })
                 .forEach(function (item) {
 
                     item.execute.apply(undefined, [item].concat(item.args || [], args));
 
-                    if (callbacks[lookUpKey] && !item.repeat) {
-
+                    if (callbacks[lookUpKey] && !item.repeat)
                         callbacks[lookUpKey].splice(callbacks[lookUpKey].indexOf(item), 1);
-
-                    }
-
-                })
-
-            }
-
+                });
         },
 
         remove: function(flowKey) {
-
-            var lookUpKey = manywho.utils.getLookUpKey(flowKey);
-
+            const lookUpKey = manywho.utils.getLookUpKey(flowKey);
             callbacks[lookUpKey] = null;
-
         }
-
-    }
+    };
 
 })(manywho);
