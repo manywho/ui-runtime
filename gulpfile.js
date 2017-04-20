5c1dcf22447d4e94f33896efac713558d5f99505
Matt Bates <matt.bates@manywho.com>
2017-04-20 14:47:04 +0100

    CORE-3169 - Added deploy tasks. Simplified build tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var runSequence = require('run-sequence');
 var del = require('del');
 var argv = require('yargs').argv;
 var path = require('path');
+var filelist = require('gulp-filelist');
 
 function getTask(task) {
     return require('./gulp-tasks/' + task)(gulp, plugins, argv);
+}
+
+function getDeployTask(task, cacheControl, src) {
+    return require('./gulp-tasks/deploy/' + task)(gulp, plugins, argv, cacheControl, src);
 }
 
 // Hooks
@@ -20,4 +24,19 @@ gulp.task('watch', ['dev-ts'], function() {
 });
 
 // Dist
-gulp.task('dist', getTask('dist/ts'));
+gulp.task('dist-clean', function() {
+    return del('./dist/**/*');
+})
+
+gulp.task('dist-hashes', ['dist-ts'], function() {
+    return gulp.src(['./dist/js/*.js'])
+        .pipe(filelist('ui-core-hashes.json', { relative: true }))
+        .pipe(gulp.dest('./dist'));  
+});
+
+gulp.task('dist-ts', ['dist-clean'], getTask('dist/ts'));
+gulp.task('dist', ['dist-hashes']);
+
+// Deploy
+gulp.task('deploy-assets', getDeployTask('cdn', 'max-age=315360000, no-transform, public', ['dist/js/*.js', 'dist/js/*.js.map']));
+gulp.task('deploy-hashes', getDeployTask('cdn', 'no-cache', ['dist/ui-core-hashes.json']));
