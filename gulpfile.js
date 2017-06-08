5b254b646c9715206f03a816cfb986e018db8c79
Matt Bates <matt.bates@manywho.com>
2017-06-08 11:01:04 +0100

    CORE-3169 - Now generating a named "bundle" manifest, removed deploy specific gulp tasks as thats now handled internally

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,6 @@ function getTask(task) {
     return require('./gulp-tasks/' + task)(gulp, plugins, argv);
 }
 
-function getDeployTask(task, cacheControl, src) {
-    return require('./gulp-tasks/deploy/' + task)(gulp, plugins, argv, cacheControl, src);
-}
-
 // Hooks
 gulp.task('pre-commit', getTask('hooks/pre-commit'));   
 
@@ -23,24 +19,21 @@ gulp.task('watch', ['dev-ts'], function() {
 });
 
 // Dist
-gulp.task('dist-clean', function() {
-    return del('./dist/**/*');
-})
+gulp.task('dist-ts', ['dist-clean'], getTask('dist/ts'));
 
-gulp.task('dist-hashes', function() {
+gulp.task('dist-bundle', ['dist-ts'], function() {
     return gulp.src(['js/*.js'], { cwd: './dist' })
-        .pipe(plugins.filelist('ui-core-hashes.json'))
-        .pipe(plugins.jsonEditor(function(hashes) {
-            return hashes.map(function(hash) {
-                return '/' + hash;
-            });
+        .pipe(plugins.filelist('bundle.json'))
+        .pipe(plugins.jsonEditor(resources => {
+            return {
+                'core': resources.map(resource => '/' + resource)
+            }
         }))
         .pipe(gulp.dest('./dist'));  
 });
 
-gulp.task('dist-ts', ['dist-clean'], getTask('dist/ts'));
-gulp.task('dist', ['dist-hashes']);
+gulp.task('dist-clean', function() {
+    return del('./dist/**/*');
+});
 
-// Deploy
-gulp.task('deploy-assets', getDeployTask('cdn', 'max-age=315360000, no-transform, public', ['dist/js/*.js', 'dist/js/*.js.map']));
-gulp.task('deploy-hashes', getDeployTask('cdn', 'no-cache', ['dist/ui-core-hashes.json']));
+gulp.task('dist', ['dist-bundle']);
