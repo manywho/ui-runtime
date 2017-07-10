1034ac2d94ea2f6c8be1c826189f03f33f773c25
Matt Bates <matt.bates@manywho.com>
2017-07-10 10:22:43 +0100

    Added specific config for npm publishing

---
+++ webpack.npm.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+var config = {
+    entry: {
+        'ui-core': './js/index.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, 'publish/js'),
+        filename: 'manywho-ui-core.js',
+        libraryTarget: 'umd',
+        library: ['manywho', 'core'],
+        umdNamedDefine: true
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    devtool: 'source-map',
+    plugins: [
+        new UglifyJSPlugin({
+            minimize: true,
+            sourceMap: true,
+            include: /\.min\.js$/,
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                enforce: 'pre',
+                loader: 'tslint-loader',
+                options: {
+                    emitErrors: true,
+                    failOnHint: true,
+                    fix: true
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                exclude: /node_modules/,
+                query: {
+                    declaration: false,
+                }
+            }
+        ]
+    }
+}
+
+module.exports = config;
