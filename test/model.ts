5e39ee2395ad3f83ffe781e660151ac6bf74f40b
Matt Bates <matt.bates@manywho.com>
2017-09-06 12:40:25 +0100

    Added some tests for the Model service

--- test/model.ts
+++ test/model.ts
@@ -114,3 +114,91 @@ test('Parse Navigation Response', (t) => {
     Model.parseNavigationResponse('id', response, flowKey, 'currentMapElementId');
     t.deepEqual(Model.getNavigation('id', flowKey), expected);
 });
+
+test('Set Components', (t) => {
+    const containers = [
+        {
+            id: 'container-id'
+        }
+    ]
+
+    Model.setContainers(flowKey, containers, null, null);
+
+    const components = [
+        {
+            attributes: {
+                key: 'value'
+            },
+            id: 'id',
+            pageContainerId: 'container-id',
+            contentType: null
+        }
+    ];
+
+    const componentsData = [
+        {
+            pageComponentId: 'id',
+            contentValue: 'value'
+        }
+    ];
+
+    Model.setComponents(flowKey, components, componentsData);
+
+    const expected = {
+        attributes: {
+            key: 'value'
+        },
+        id: 'id',
+        pageComponentId: 'id',
+        pageContainerId: 'container-id',
+        contentType: 'CONTENTSTRING',
+        contentValue: 'value'
+    }
+
+    t.deepEqual(Model.getComponent('id', flowKey), expected);
+});
+
+test('Set History', (t) => {
+    const response = {
+        mapElementInvokeResponses: [
+            {
+                developerName: 'response',
+                mapElementId: 'id',
+                pageResponse: {
+                    label: 'label',
+                    pageComponentDataResponses: [
+                        {
+                            content: 'content'
+                        }
+                    ]
+                },
+                outcomeResponses: [
+                    {
+                        developerName: 'outcome',
+                        id: 'outcome-id',
+                        label: 'outcome',
+                        order: 1
+                    }
+                ]
+            }
+        ]
+    }
+
+    const expected = [{
+        name: response.mapElementInvokeResponses[0].developerName,
+        id: response.mapElementInvokeResponses[0].mapElementId,
+        label: response.mapElementInvokeResponses[0].pageResponse.label,
+        content: response.mapElementInvokeResponses[0].pageResponse.pageComponentDataResponses[0].content,
+        outcomes: [
+            {
+                name: 'outcome',
+                id: 'outcome-id',
+                label: 'outcome',
+                order: 1
+            }
+        ]
+    }];
+
+    Model.setHistory(response, flowKey);
+    t.deepEqual(Model.getHistory(flowKey), expected);
+});
