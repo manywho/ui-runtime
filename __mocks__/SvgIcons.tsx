f04d2b84ade34d2dba40fdef059dd1de8f40ab90
Ian Haycox <ianhaycox@boomi.com>
2020-06-22 10:38:37 +0100

    FLOW-1619 Add svg export

--- __mocks__/SvgIcons.tsx
+++ __mocks__/SvgIcons.tsx
@@ -1,7 +1,11 @@
 import React, { ReactElement } from 'react';
 
+/*
+ * In order to avoid React errors we need to export the two 'mocked'
+ * Svg icons
+ */
 
 const OfflineSvg = (): ReactElement => <div />;
-const OnlineSvg = (): ReactElement => <div />;
+export const OnlineSvg = (): ReactElement => <div />;
 
 export default OfflineSvg;
