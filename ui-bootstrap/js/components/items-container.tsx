dcb38f50bfc3a461e36989d5e6de06cb33a4e5f0
Dale Shoulders <dshoulders@outlook.com>
2020-11-30 12:13:17 +0000

    FLOW-2382 - Re-add missing if brace

--- ui-bootstrap/js/components/items-container.tsx
+++ ui-bootstrap/js/components/items-container.tsx
@@ -397,6 +397,7 @@ class ItemsContainer extends React.Component<IComponentProps, IItemsContainerSta
             // Sort the filtered list before slicing
             if (this.state.sortedBy && Array.isArray(objectData)) {
                 objectData.sort(this.compare(this.state.sortedBy, this.state.sortedIsAscending));
+            }
 
             if (
                 model.attributes.pagination &&
