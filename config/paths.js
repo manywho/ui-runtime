bcf7cb038af77fd41b1fe37dcadfedb39f97fc93
Dale Shoulders <dshoulders@outlook.com>
2020-12-16 16:00:19 +0000

    FLOW-2823 - Remove ui-vendor and ui-html5 directories and some other unused legacy stuff

--- config/paths.js
+++ config/paths.js
@@ -1,9 +1,7 @@
 const repoPaths = {
     build: `./runtime_build`,
     uiCore: './ui-core',
-    uiHtml5: './ui-html5',
     uiThemes: './ui-themes',
-    uiVendor: './ui-vendor',
     uiOffline: './ui-offline',
     uiBootstrap: './ui-bootstrap',
 };
--- update-ui-bootstrap.sh
+++
@@ -1,6 +0,0 @@
-# 1 add sub-repo's remote
-git remote add -f ui-bootstrap git@github.com:manywho/ui-bootstrap.git
-# 2 subtree merge the changes
-git pull -s subtree -Xsubtree=ui-bootstrap ui-bootstrap develop --allow-unrelated-histories
-# 3 remove sub-repo's remote again
-git remote rm ui-bootstrap
