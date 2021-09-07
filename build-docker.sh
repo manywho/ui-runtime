c4bedd5fe1ffd6293a328071e6d75cc48e1c0e52
roblilleker <robert.lilleker@boomi.com>
2021-09-07 15:13:39 +0100

    migrate to ecr

--- build-docker.sh
+++ build-docker.sh
@@ -5,6 +5,8 @@ set -e
 FLOW_ECR_REGISTRY="093652424831.dkr.ecr.eu-west-2.amazonaws.com"
 BRANCH_NAME="${bamboo_planRepository_branchName}"
 
+#docker login --username AWS --password "${bamboo_planRepository_branchName}" "${FLOW_ECR_REGISTRY}"
+
 if [ "$BRANCH_NAME" = "" ]; then
   echo "No bamboo_planRepository_branchName environment variable is set" 1>&2
   exit 1
