f9f3ba27e880d646562c12708ed31c4fd1903b77
jamesbrattBoomi <james_bratt@dell.com>
2022-05-13 09:25:18 +0100

    FLOW-5056 - Parsing number values as floating point integers

--- ui-offline/js/services/Operation.ts
+++ ui-offline/js/services/Operation.ts
@@ -148,7 +148,7 @@ export const executeOperation = (operation: any, state: IState, snapshot: any) =
                 }
 
                 if (isNumericalOperation) {
-                    valueToReference.contentValue = parseInt(valueToReference.contentValue, 10) + parseInt(valueToApply.contentValue, 10)
+                    valueToReference.contentValue = parseFloat(valueToReference.contentValue) + parseFloat(valueToApply.contentValue)
                 }
 
                 if (isListOperation) {
